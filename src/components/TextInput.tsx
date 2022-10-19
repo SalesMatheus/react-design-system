f428e903e9432620a524c688a61c0378e3da0115
Matheus Sales <matheus.carvalho.sales@gmail.com>
2022-10-19 09:39:44 -0300

    feat: add component input

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -1,15 +1,34 @@
-import { InputHTMLAttributes } from 'react';
+import { InputHTMLAttributes, ReactNode } from 'react';
 import { clsx } from 'clsx';
 
-export interface TextInputProps extends InputHTMLAttributes<HTMLInputElement> {}
+export interface TextInputInputProps extends InputHTMLAttributes<HTMLInputElement> {}
 
-export function TextInput(props: TextInputProps) {
+export interface TextInputRootProps {
+  children: ReactNode,
+}
 
+function TextInputRoot(props: TextInputRootProps) {
   return (
-    <input className={clsx(
-      'py-4 px-3 rounded bg-gray-800 w-full text-gray-100 text-xs placeholder:text-gray-400',
-    )}
+    <div className={clsx(
+      'flex items-center gap-3 py-4 px-3 rounded w-full bg-gray-800 focus-within:ring-2 ring-gray-400',
+    )}>
+      {props.children}
+    </div>
+  )
+}
+
+function TextInputIcon() {}
+
+function TextInputInput(props: TextInputInputProps) {
+  return (
+    <input className="bg-transparent outline-none flex-1 text-gray-100 text-xs placeholder:text-gray-400"
     {...props}
     />
   );
+}
+
+export const TextInput = {
+  Root: TextInputRoot,
+  Input: TextInputInput,
+  Icon: TextInputIcon
 }
